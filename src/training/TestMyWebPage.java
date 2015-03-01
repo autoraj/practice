5f23610c6ec6c96fe8c00edc98a9261514da87f6
anbu <anburaj@gmail.com>
2015-02-28 21:45:28 -0500

    sample code

---
+++ src/training/TestMyWebPage.java
@@ -0,0 +1,37 @@
+package training;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestMyWebPage {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.ie.driver", "C:\\Anbu\\Training\\IEDriverServer.exe");
+		WebDriver driver = new InternetExplorerDriver();
+		driver.get("http://www.jesuscalls.com/");
+		Thread.sleep(1000);
+		System.out.println("Title:"+driver.getTitle());
+		String strPromise = driver.findElement(By.className("home_news_link_text")).getText();
+		System.out.println(strPromise);
+		driver.findElement(By.className("home_news_link_text")).sendKeys(Keys.ENTER);
+		//driver.findElement(By.name("btnK")).click();
+		 String strPromise1 = driver.findElement(By.cssSelector("p")).getText();
+		 System.out.println(strPromise1);
+		driver.quit();
+	}
+	
+	
+
+}
